850d1fa9ac98b9ae761765ba99657298e4f8967f
WalterGS87 <waltergustavososa@gmail.com>
2023-05-23 19:52:07 -0300

    Add files via upload
    nueva subida

--- main.js
+++ main.js
@@ -1,25 +1,57 @@
-function convertirPesos() {
-  
-    var monto = parseFloat(document.getElementById("monto").value);
-    var dolarBlue = 400;
-    var dolarOficial = 200;
-    
+var DolarCity = {
+  conversion: [
+    { tipo: "Dólar Blue", valor: 400 },
+    { tipo: "Dólar Oficial", valor: 200 }
+  ],
+  convertir: function (monto) {
+    var resultado = "";
     if (monto > 1000) {
-        
-        alert
-    
-       
-    alert("NO SE PUEDE CONVERTIR");
-      } 
-     
-    else {
-        
-    var dolaresBlue = monto / dolarBlue;
-               
-    var dolaresOficial = monto / dolarOficial;
-    
-    document.getElementById("resultadoBlue").innerHTML = "Monto en dólares (Dólar Blue): " + dolaresBlue;
-        document.getElementById("resultadoOficial").innerHTML = "Monto en dólares (Dólar Oficial): " + dolaresOficial;
+      alert("NO SE PUEDE CONVERTIR. Monto superior a 1000.");
+    } else {
+      for (var i = 0; i < this.conversion.length; i++) {
+        var moneda = this.conversion[i];
+        var conversion = monto / moneda.valor;
+        resultado += "Monto en " + moneda.tipo + ": " + conversion + "<br>";
       }
     }
-    
+    return resultado;
+  }
+};
+
+var EuroCity = {
+  conversion: [
+    { tipo: "Euro Blue", valor: 500 },
+    { tipo: "Euro Oficial", valor: 300 }
+  ],
+  convertir: function (monto) {
+    var resultado = "";
+    if (monto > 1000) {
+      alert("NO SE PUEDE CONVERTIR. Monto superior a 1000.");
+    } else {
+      for (var i = 0; i < this.conversion.length; i++) {
+        var moneda = this.conversion[i];
+        var conversion = monto / moneda.valor;
+        resultado += "Monto en " + moneda.tipo + ": " + conversion + "<br>";
+      }
+    }
+    return resultado;
+  }
+};
+
+function convertirMoneda() {
+  var monto = parseFloat(document.getElementById("monto").value);
+
+  var selectMoneda = document.getElementById("selectMoneda");
+  var tipoMoneda = selectMoneda.options[selectMoneda.selectedIndex].value;
+
+  var resultadoDiv = document.getElementById("resultado");
+  resultadoDiv.innerHTML = "";
+
+  if (tipoMoneda === "dolar") {
+    var resultado = DolarCity.convertir(monto);
+    resultadoDiv.innerHTML = resultado;
+  } else if (tipoMoneda === "euro") {
+    var resultado = EuroCity.convertir(monto);
+    resultadoDiv.innerHTML = resultado;
+  }
+}
